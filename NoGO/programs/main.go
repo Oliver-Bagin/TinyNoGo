8c3e541e2f20f36c6238f2fc8a921e9b5f6e22c2
parth-io <67057645+parth-io@users.noreply.github.com>
2025-04-22 17:18:50 +0800

    Update main.go

--- NoGO/programs/main.go
+++ NoGO/programs/main.go
@@ -1,28 +1,27 @@
 package main
 
-go:wasmimport yourmodulename add
-func add(x, y uint32) uint32
-
-// function test() {
-//     println("Calling a 'function'")
-// }
+func add(x, y uint32) uint32 {
+    return x + y
+}
 
 func main() {
     result := add($10, 32)
-    // test()
     t := 2
-    q := t == 2 ? "A" : (t == 1 ? "B" : "C")
-    z := t == 2 ? "A" : t == 1 ? "B" : "C"
-    x := t == 2 ? (t == 1 ? "A" : "B") : "C"
-    // y := t == 2 ? t == 1 ? "A" : "B" : "C"
+    // a := t == 2 ? "A" : "B"
+    // q := t == 2 ? "A" : (t == 1 ? "B" : "C")
+    // z := t == 2 ? "A" : t == 1 ? "B" : "C"
+    // x := t == 2 ? (t == 1 ? "A" : "B") : "C"
+    y := t == 2 ? t == 1 ? "A" : "B" : "C"
     // e := (t == 1 ? true : false) ? "A" : "C"
     // ww = t == 1 ? true : false ? "A" : "C"
 
-    println("Result from JS:", add)
-    println("Result from JS:", q)
-    println("Result from JS:", z)
-    println("Result from JS:", x)
-    // println("Result from JS:", y)
+    // test triply nested
+    println("Result from JS:", result)
+    // println("Result from JS:", a)
+    // println("Result from JS:", q)
+    // println("Result from JS:", z)
+    // println("Result from JS:", x)
+    println("Result from JS:", y)
     // println("Result from JS:", e)
     // println("Result from JS:", ww)
 }
